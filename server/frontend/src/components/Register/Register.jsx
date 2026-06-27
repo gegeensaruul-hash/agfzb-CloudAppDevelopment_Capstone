f9d6787999bcd99b196caa2b6bdae2306618c4e6
gegeensaruul <gegeensaruul@gmail.com>
2026-06-27 12:01:58 +0800

    Add form element to Register component

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-
 function Register() {
   const [username, setUsername] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const handleRegister = async () => {
+  const handleRegister = async (e) => {
+    e.preventDefault();
     const response = await fetch('/djangoapp/registration', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -20,18 +19,18 @@ function Register() {
       alert(data.error || 'Registration failed');
     }
   };
-
   return (
     <div>
       <h1>Sign Up</h1>
-      <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} /><br/>
-      <input type="text" placeholder="First Name" value={firstName} onChange={e => setFirstName(e.target.value)} /><br/>
-      <input type="text" placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)} /><br/>
-      <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} /><br/>
-      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} /><br/>
-      <button onClick={handleRegister}>Register</button>
+      <form onSubmit={handleRegister}>
+        <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} /><br/>
+        <input type="text" placeholder="First Name" value={firstName} onChange={e => setFirstName(e.target.value)} /><br/>
+        <input type="text" placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)} /><br/>
+        <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} /><br/>
+        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} /><br/>
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
-
-export default Register;	
+export default Register;
